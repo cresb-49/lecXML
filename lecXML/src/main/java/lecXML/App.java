16b3d006d735388eadd10bbcedfb6157c1c9c334
Benjamin <carlosbpac@gmail.com>
2020-12-02 02:16:37 -0600

    Actualizacion de funcionameinto

--- lecXML/src/main/java/lecXML/App.java
+++ lecXML/src/main/java/lecXML/App.java
@@ -1,13 +1,10 @@
 package lecXML;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import lecXML.analicisLexico.ErrorLexico;
 import lecXML.analicisLexico.Token;
